fc76c4a5851eb9ad4e5e1c14cfe3afe936d3a7f2
ybr <yohann.bredoux@gmail.com>
2016-10-24 17:51:03 +0200

    First commit

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [ -z $AUTHORIZED_KEYS ]; then
+  echo "You must provide the environment variable AUTHORIZED_KEYS"
+  exit 1
+fi
+
+echo "Creating /root/.ssh/authorized_keys file..."
+
+mkdir -p /root/.ssh
+echo "$AUTHORIZED_KEYS" > /root/.ssh/authorized_keys
+
+/usr/sbin/sshd -D
